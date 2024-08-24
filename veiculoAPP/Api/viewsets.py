112f153883589aeef90f3e78c9ae768874aaf964
MarcuSantosDev <marcus.xtreme03@gmail.com>
2024-08-24 19:24:57 -0300

    add CarroModelo no models e viewset carro

--- veiculoAPP/Api/viewsets.py
+++ veiculoAPP/Api/viewsets.py
@@ -1,7 +1,35 @@
 from .serializers import MarcaSerializer
 from ..models import MarcaModel
 from rest_framework.viewsets import ModelViewSet
+import requests
+from rest_framework.response import Response
 
-class MarcaModelViewSet(ModelViewSet):
+''''class MarcaModelViewSet(ModelViewSet):
     queryset = MarcaModel.objects.all()
     serializer_class = MarcaSerializer
+
+'''
+
+class MarcaModelViewSet(ModelViewSet):   # se dar um post na rota desse viewset vai cadastrar todas as marcas no Banco de dados
+    queryset = MarcaModel.objects.all()
+    serializer_class = MarcaSerializer    
+    
+    def create(self, request):
+        site = (f'https://parallelum.com.br/fipe/api/v1/carros/marcas')
+        requisicao = requests.get(site)
+        json = requisicao.json()
+        
+        marcasSalvas = []
+        for item in json:
+            codigo = item.get('codigo', '')
+            nome = item.get('nome', '')
+
+            dadosrecebidos = {
+                "codigo": f"{codigo}",
+                "nome": f"{nome}", 
+            }
+        
+            meuserializer = MarcaSerializer(data=dadosrecebidos)
+            if meuserializer.is_valid():
+                meuserializer.save()
+                marcasSalvas.append(meuserializer.data)
--- veiculoAPP/models.py
+++ veiculoAPP/models.py
@@ -6,4 +6,9 @@ class MarcaModel(models.Model) :
     codigo = models.IntegerField(blank=True, null=True);
     nome = models.CharField(max_length=256,blank=True, null=True )
 
-    
+class CarroModeloModel(models.Model):
+    codigoMarca = models.ForeignKey('MarcaModel', models.DO_NOTHING, blank=True, null=True)
+    codigo = models.CharField(max_length=75)   
+    nome = models.CharField(max_length=75)
+
+
